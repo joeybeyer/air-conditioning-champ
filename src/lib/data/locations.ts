1ef0d5dacf9b1baaa7887c7cad72001c37c8a64f
joeybeyer <joey@seologicagency.com>
2026-02-24 16:20:22 -0600

    Add El Mirage map embed

--- src/lib/data/locations.ts
+++ src/lib/data/locations.ts
@@ -73,6 +73,7 @@ export const LOCATIONS: Location[] = [
       state: 'AZ',
       zip: '85335',
     },
+    googleMapsEmbed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3322.324091500123!2d-112.3402034!3d33.622837800000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b45ee33b83335%3A0xd2bd11afde6dc129!2sAir%20Conditioning%20Champ!5e0!3m2!1sen!2sus!4v1771971534262!5m2!1sen!2sus',
   },
   {
     slug: 'surprise-az',
